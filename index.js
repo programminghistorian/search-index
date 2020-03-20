7a86691d156b5691482dd27a17d067242ffbcb3b
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2020-03-20 10:19:05 -0400

    updated code to work for large json files and multiple languaes

--- index.js
+++ index.js
@@ -2,21 +2,33 @@
 const lunr = require('lunr');
 const fs = require('fs');
 
-let searchEn = require('./search.json');
-const idxEn = lunr((builder) => {
-    builder.ref('id');
-    builder.field('title');
-    builder.field('body');
-    builder.metadataWhitelist = ['position']
+let searchCorpora = ['./fr/search.json', './es/search.json', './en/search.json'];
 
-    searchEn.forEach(function (doc) {
-        builder.add(doc)
-    }, builder)
+searchCorpora.map(searchFile => {
+    let language = searchFile.split('/').reverse()[1].toUpperCase();
+    fs.readFile(searchFile, 'utf8', (err, data) => {
+        if (err) throw err;
+        let searchBuilder = JSON.parse(data);
+
+        const idx = lunr((builder) => {
+            builder.ref('id');
+            builder.field('title');
+            builder.field('body');
+            builder.metadataWhitelist = ['position']
+
+            searchBuilder.forEach(function (doc) {
+                builder.add(doc)
+            }, builder)
+        });
+
+        let dir = './indices';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        fs.writeFileSync(`./indices/index${language}.json`, JSON.stringify(idx));
+
+    });
 });
 
-let dir = './indices';
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-}
 
-fs.writeFileSync('./indices/indexEN.json', JSON.stringify(idxEn));
