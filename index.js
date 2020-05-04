c109facc14394702557d4b692a22142a0aeb3364
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2020-05-04 19:20:10 -0400

    updates to actually use stemmers for each language

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
-const lunr = require('lunr');
+const lunr = require('lunr')
+require("lunr-languages/lunr.stemmer.support")(lunr)
+require('lunr-languages/lunr.fr')(lunr)
+require('lunr-languages/lunr.es')(lunr)
+
 const fs = require('fs');
 const axios = require('axios');
+
 
 let searchCorpora = ['https://programminghistorian.org/en/search.json', 'https://programminghistorian.org/fr/search.json', 'https://programminghistorian.org/es/search.json'];
 
 searchCorpora.map(searchFile => {
-    let language = searchFile.split('/').reverse()[1].toUpperCase();
+    let language = searchFile.split('/').reverse()[1];
+
     axios.get(searchFile).then((response)=>{
         let searchBuilder = response.data;
         const idx = lunr((builder) => {
+            language != 'en' ? builder.use(lunr[language]) : null;
             builder.ref('id');
             builder.ref('url');
             builder.field('title');
@@ -26,7 +33,7 @@ searchCorpora.map(searchFile => {
             fs.mkdirSync(dir);
         }
 
-        fs.writeFileSync(`./indices/index${language}.json`, JSON.stringify(idx));
+        fs.writeFileSync(`./indices/index${language.toUpperCase()}.json`, JSON.stringify(idx));
     }).catch((err) => {
         console.log(err);
     });
